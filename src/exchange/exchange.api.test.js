5cc79757dbbc19bfe6cfb382734d57892eaa494f
rod <rod.a.nato@gmail.com>
2018-10-10 23:00:47 -0500

    feat(exchange.api): Unit test

--- src/exchange/exchange.api.test.js
+++ src/exchange/exchange.api.test.js
@@ -1,21 +1,15 @@
 import {doExchangeFromTo} from './exchange.api';
 
 describe('Exchange API', () => {
-  it('Returns the conversion rate', () => {
+  it('Should return the conversion rate', () => {
     return doExchangeFromTo('USD', 'EUR')
-    .then(res => res.json())
-    .then(res => expect(res['USD_EUR'].val).toBeDefined()); 
+      .then(res => res.json())
+      .then(res => expect(res['USD_EUR']).toBeDefined()); 
   });
 
-  it('should reject if there is no parameters', async() => {
-    // return doExchangeFromTo('USD', 'EUR')
-    // .catch(e => expect(e).toBeTruthy());
-    expect.assertions(1);
-
-    try {
-      await doExchangeFromTo()
-    } catch (e) {
-      expect(e).toBeTruthy()
-    }
+  it('Should fail if the parameters are not well settled', () => {
+    return doExchangeFromTo('xxx', 'yyy')
+      .then(res => res.json())
+      .then(res => expect(res['USD_EUR']).toBeUndefined()); 
   });
 });
